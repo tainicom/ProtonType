a63ca0554607e95d6c40dd8b4d57d9e75222a8a6
Nikos Kastellanos <nkastellanos@gmail.com>
2017-07-16 16:34:34 +0300

    Add ProtonType.FileSystem

---
+++ ProtonType.FileSystem/ViewModels/BrowserItem.cs
@@ -0,0 +1,76 @@
+﻿#region License
+//   Copyright 2017 Kastellanos Nikolaos
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using tainicom.ProtonType.Framework.Helpers;
+
+namespace tainicom.ProtonType.ViewModels
+{
+    public class BrowserItem : NotificationObject, IComparable<BrowserItem>
+    {
+        public readonly FileBrowser FileBrowser;
+        protected string _relativePath;
+        
+        string _name;
+        string _ext;
+
+        public string Testing { get; set; }
+
+        public virtual string Name { get { return _name; } } 
+        public virtual string RelativePath { get { return _relativePath; } }
+
+        public virtual IList<BrowserItem> Items { get { return null; } } 
+        
+
+        
+        internal protected BrowserItem(FileBrowser fileBrowser, string relativePath)
+        {
+            this.FileBrowser = fileBrowser;
+            this._relativePath = relativePath;
+        
+            this._name = Path.GetFileName(relativePath);
+            this._ext = Path.GetExtension(this._name);
+        }
+        
+        internal void Rename(string newFilename)
+        {
+            this._relativePath = Path.Combine(Path.GetDirectoryName(this._relativePath), newFilename);
+            this._name = Path.GetFileName(newFilename);
+            this._ext = Path.GetExtension(this._name);
+            RaisePropertyChanged(() => Name);
+        }
+        
+        #region IComparable
+        public int CompareTo(BrowserItem other)
+        {
+            int res = 0;
+            res = ((other is FolderItem) ? 1 : 0) - ((this is FolderItem) ? 1 : 0);
+            if (res == 0) res = _relativePath.CompareTo(other._relativePath);
+            if (res == 0) res = _name.CompareTo(other._name);
+               
+            return res;
+        }
+        #endregion
+
+        public override string ToString()
+        {
+            return String.Format("{{{0}, Name: {1}}}", GetType().Name, _name);
+        }
+
+    }
+}
---
+++ ProtonType.FileSystem/ViewModels/FileBrowser.cs
@@ -0,0 +1,51 @@
+﻿#region License
+//   Copyright 2017 Kastellanos Nikolaos
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+#endregion
+
+using System.Collections.Generic;
+using System.IO;
+using tainicom.ProtonType.Framework.ViewModels;
+
+namespace tainicom.ProtonType.ViewModels
+{
+    public partial class FileBrowser : ToolViewModel
+    {
+        private string _absolutePath;
+        FolderItem _rootItem;
+        public IList<BrowserItem> Items { get { return _rootItem.Items; } }
+        
+        public FileBrowser(object model, string name, string absolutePath)
+            : base(model, name)
+        {
+            this._absolutePath = absolutePath;
+            _rootItem = this.CreateFolderItem("");            
+        }
+
+        public virtual FolderItem CreateFolderItem(string relativePath)
+        {
+            return new FolderItem(this, relativePath);
+        }
+
+        public virtual BrowserItem CreateBrowserItem(string relativePath)
+        {
+            return new BrowserItem(this, relativePath);
+        }
+
+        public virtual string GetAbsolutePath(string relativePath)
+        {
+            return Path.Combine(_absolutePath, relativePath);
+        }
+    }
+}
