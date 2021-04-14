3ed641549a0d26349b1d63046af2100bd47c54c2
Nick Kastellanos <nkastellanos@gmail.com>
2021-04-14 16:29:35 +0300

    Add AvalonDock

---
+++ 3rdPartyLibraries/AvalonDock/src/AvalonDock/Events/AnchorableHiddenEventArgs.cs
@@ -0,0 +1,31 @@
+﻿/************************************************************************
+
+   AvalonDock
+
+   Copyright (C) 2019 Kastellanos Nikos
+
+   This program is provided to you under the terms of the New BSD
+   License (BSD) as published at http://avalondock.codeplex.com/license 
+
+   For more features, controls, and fast professional support,
+   pick up AvalonDock in Extended WPF Toolkit Plus at http://xceed.com/wpf_toolkit
+
+   Stay informed: follow @datagrid on Twitter or Like facebook.com/datagrids
+
+  **********************************************************************/
+
+using System;
+using AvalonDock.Layout;
+
+namespace AvalonDock
+{
+    public class AnchorableHiddenEventArgs : EventArgs
+    {
+        public readonly LayoutAnchorable Anchorable;
+
+        public AnchorableHiddenEventArgs(LayoutAnchorable anchorable)
+        {
+            Anchorable = anchorable;
+        }
+    }
+}
